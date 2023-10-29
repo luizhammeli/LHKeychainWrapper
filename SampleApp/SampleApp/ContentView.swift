c78d6a38d1520e1b2a0036f62f4b1b97c31c2743
Luiz Hammerli <luizhammerli@gmail.com>
2023-10-29 00:28:54 -0300

    Update ContentView.swift

--- SampleApp/SampleApp/ContentView.swift
+++ SampleApp/SampleApp/ContentView.swift
@@ -19,6 +19,12 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
+//#Preview {
+//    ContentView()
+//}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
